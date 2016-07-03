634941aabd5d5be286b140236fe64e017c8f359f
Carey Li <careyliau@gmail.com>
2016-07-03 19:40:46 +1000

    Basic functionality

--- index.php
+++ index.php
@@ -1,11 +1,17 @@
 <?php
 
     $routes = array(
-        "default" => "marketcap"
+        "marketcap" => "marketcap",
+        "detailed_overview" => "detailed_overview",
+        "stock_search" => "stock_search",
+        "stock_view" => "stock_view"
     );
 
     $descriptions = array(
-        "portfolio" => "Total value of your holdings."
+        "portfolio" => "Total value of your holdings.",
+        "detailed_overview" => "Detailed overview of your owned stock and options.",
+        "stock_search" => "Search for stocks by symbol",
+        "stock_view" => "View historical data & ownership information"
     );
 
     if (!isset($_GET['r']) || !in_array($_GET['r'], $routes)) {
@@ -22,10 +28,9 @@
 <html lang="en">
     <!--<![endif]-->
     <!-- BEGIN HEAD -->
-
     <head>
         <meta charset="utf-8" />
-        <title>Stockmarket Simulator | </title>
+        <title>Stockmarket Simulator | <?= ucwords($route) ?></title>
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta content="width=device-width, initial-scale=1" name="viewport" />
         <meta content="" name="description" />
@@ -70,7 +75,7 @@
                             </button>
                             <!-- End Toggle Button -->
                             <!-- BEGIN LOGO -->
-                            <a id="index" class="page-logo" href="index.html">
+                            <a id="index" class="page-logo" href="?r=portfolio">
                                 <img src="assets/layouts/layout5/img/logo.png" alt="Logo"> </a>
                             <!-- END LOGO -->
                             <!-- BEGIN SEARCH
@@ -146,21 +151,35 @@
                         <!-- BEGIN HEADER MENU -->
                         <div class="nav-collapse collapse navbar-collapse navbar-responsive-collapse">
                             <ul class="nav navbar-nav">
-                                <li class="dropdown dropdown-fw  active open selected">
+                                <li class="dropdown dropdown-fw <?php 
+                                    if (in_array($route, ["portfolio", "detailed_overview"])) {
+                                        echo "active open selected";
+                                    }
+                                ?>">
                                     <a href="javascript:;" class="text-uppercase">
                                         <i class="icon-home"></i> Your Portfolio </a>
                                     <ul class="dropdown-menu dropdown-menu-fw">
                                         <li>
-                                            <a href="index.html">
+                                            <a href=".">
                                                 <i class="icon-bar-chart"></i> Overview </a>
                                         </li>
                                         <li>
-                                            <a href="dashboard_2.html">
-                                                <i class="icon-bulb"></i> Dashboard 2 </a>
+                                            <a href="?r=detailed_overview">
+                                                <i class="icon-bulb"></i> Detailed overview </a>
                                         </li>
+                                    </ul>
+                                </li>
+                                <li class="dropdown dropdown-fw <?php 
+                                    if (in_array($route, ["stock_search", "stock_view"])) {
+                                        echo "active open selected";
+                                    }
+                                ?>">
+                                    <a href="javascript:;" class="text-uppercase">
+                                        <i class="icon-book-open"></i> Stock Information </a>
+                                    <ul class="dropdown-menu dropdown-menu-fw">
                                         <li>
-                                            <a href="dashboard_3.html">
-                                                <i class="icon-graph"></i> Dashboard 3 </a>
+                                            <a href="?r=stock_search">
+                                                <i class="icon-magnifier"></i> Search </a>
                                         </li>
                                     </ul>
                                 </li>
@@ -176,8 +195,8 @@
                 <div class="page-content">
                     <!-- BEGIN BREADCRUMBS -->
                     <div class="breadcrumbs">
-                        <h1><?= ucwords($route) ?> <br> <small style="color: #696969"><?= $descriptions[$route] ?></small></h1>
-                        <!-- Sidebar Toggle Button -->
+                        <h1><?= str_replace("_", " ", $route) ?> <br> <small style="color: #696969"><?= $descriptions[$route] ?></small></h1>
+                        <!-- Sidebar Toggle Button
                         <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".page-sidebar">
                             <span class="sr-only">Toggle navigation</span>
                             <span class="toggle-icon">
@@ -186,7 +205,7 @@
                                 <span class="icon-bar"></span>
                             </span>
                         </button>
-                        <!-- Sidebar Toggle Button -->
+                        Sidebar Toggle Button -->
                     </div>
                     <!-- END BREADCRUMBS -->
                     <!-- BEGIN SIDEBAR CONTENT LAYOUT -->
@@ -806,6 +825,9 @@
         <script src="assets/global/plugins/highcharts/js/highcharts.js" type="text/javascript"></script>
         <script src="assets/global/plugins/highcharts/js/highcharts-3d.js" type="text/javascript"></script>
         <script src="assets/global/plugins/highcharts/js/highcharts-more.js" type="text/javascript"></script>
+        <script src="assets/global/plugins/typeahead/handlebars.min.js" type="text/javascript"></script>
+        <script src="assets/global/plugins/typeahead/typeahead.bundle.min.js" type="text/javascript"></script>
+
         <script src="stocksim/js/const.js"></script>
         <script src="stocksim/js/app.js"></script>
     </body>
--- stocksim/js/app.js
+++ stocksim/js/app.js
@@ -16,20 +16,9 @@ ss.routines = {
         console.info("StockSim Rev " + commit);
         console.info("Selected mode: " + ss.data.market_condition);
 
-        switch (ss.data.market_condition) {
-            case M_PEACEFUL:
-                ss.data.internal.uptick_rate = M_PEACEFUL_BOUNDARY;
-                break;
-            case M_NORMAL:
-                ss.data.internal.uptick_rate = M_NORMAL_BOUNDARY;
-                break;
-            case M_VOLATILE:
-                ss.data.internal.uptick_rate = M_VOLATILE_BOUNDARY;
-                break;
-        };
-
         $(window).on('beforeunload', function(){
-             ss.db.save_data = JSON.stringify(ss.data.player_data);
+             ss.db.player_data = JSON.stringify(ss.data.player_data);
+             ss.db.company_data = JSON.stringify(ss.data.company_data);
         });
 
         ss.ticker.startStockTickers();
@@ -48,6 +37,53 @@ ss.routines = {
         };
 
         return value;
+    },
+    stock_search: function(symbol) {
+        if (Object.keys(ss.data.company_data).indexOf(symbol) === -1) {
+            alert("Invalid symbol provided.");
+        };
+
+        window.location = "?r=stock_view&symbol=" + symbol;
+    },
+    stock_purchase: function() {
+        var amount = $("#purchase_amount").val(),
+            params={};window.location.search.replace(/[?&]+([^=&]+)=([^&]*)/gi,function(str,key,value){params[key] = value;}),
+            purchase_amount = amount * ss.data.company_data[params.symbol].last_price;
+
+        if (purchase_amount > ss.db.player_data.cash) {
+            alert("Insufficent funds");
+        } else {
+            if (confirm("Purchase " + amount + " shares of " + params.symbol + " for " + purchase_amount + "?")) {
+                if (typeof ss.data.player_data.owned_stocks[params.symbol] === "undefined") {
+                    ss.data.player_data.owned_stocks[params.symbol] = amount;
+                } else {
+                    ss.data.player_data.owned_stocks[params.symbol] = Math.floor(ss.data.player_data.owned_stocks[params.symbol]) + Math.floor(amount);
+                }
+
+                ss.data.player_data.cash -= purchase_amount;
+
+                $("#owned_shares").html(ss.data.player_data.owned_stocks[params.symbol]);
+                $("#cash").html(ss.data.player_data.cash);
+            }
+        }
+    },
+    stock_sell: function() {
+        var amount = $("#sell_amount").val(),
+            params={};window.location.search.replace(/[?&]+([^=&]+)=([^&]*)/gi,function(str,key,value){params[key] = value;}),
+            sell_amount = amount * ss.data.company_data[params.symbol].last_price;
+
+        if (Math.floor(amount) > ss.data.player_data.owned_stocks[params.symbol]) {
+            alert("You cannot sell more stocks than you own.")
+        } else {
+            if (confirm("Sell " + amount + " shares of " + params.symbol + " for " + sell_amount + "?")) {
+                ss.data.player_data.owned_stocks[params.symbol] = Math.floor(ss.data.player_data.owned_stocks[params.symbol]) - Math.floor(amount);
+
+                ss.data.player_data.cash += sell_amount;
+
+                $("#owned_shares").html(ss.data.player_data.owned_stocks[params.symbol]);
+                $("#cash").html(ss.data.player_data.cash);
+            }
+        }
     }
 }
 
@@ -62,11 +98,11 @@ ss.ticker = {
 
             ss.data.internal.handles[code] = setInterval(function() {
                     
-                Math.random() <= (ss.data.internal.uptick_rate + (ss.data.company_data[code].last_price <= 60 ? 0.2 : 0)) ? ss.data.internal.movement = M_UP : ss.data.internal.movement = M_DOWN;
+                Math.random() <= (M_SETTINGS[ss.data.market_condition].boundary + (ss.data.company_data[code].last_price <= 60 ? 0.2 : 0)) ? ss.data.internal.movement = M_UP : ss.data.internal.movement = M_DOWN;
 
                 ss.data.company_data[code].last_price += ss.rng.movement(ss.data.internal.movement);
 
-            }, 5000);
+            }, 10000);
         };
         
     },
@@ -85,14 +121,7 @@ ss.ticker = {
 
 ss.rng = {
     movement: function(direction) {
-        switch(ss.data.market_condition) {
-            case M_PEACEFUL:
-                return direction * (Math.floor(Math.random() * (M_PEACEFUL_MAXIMUM_MOVE - M_PEACEFUL_MINIMUM_MOVE)) + M_PEACEFUL_MAXIMUM_MOVE);
-            case M_NORMAL:
-                return direction * (Math.floor(Math.random() * (M_NORMAL_MAXIMUM_MOVE - M_NORMAL_MINIMUM_MOVE)) + M_NORMAL_MAXIMUM_MOVE);
-            case M_VOLATILE:
-                return direction * (Math.floor(Math.random() * (M_VOLATILE_MAXIMUM_MOVE - M_VOLATILE_MINIMUM_MOVE)) + M_VOLATILE_MAXIMUM_MOVE);
-        };
+        return direction * (Math.floor(Math.random() * (M_SETTINGS[ss.data.market_condition].max_move - M_SETTINGS[ss.data.market_condition].min_move)) + M_SETTINGS[ss.data.market_condition].max_move);
     }
 }
 
@@ -110,8 +139,8 @@ ss.pages = {
                                     series = chart.series[0],
                                     shift = series.data.length > 20;
 
-                                chart.series[0].addPoint([Date.now(), ss.routines.getPortfolioValue()], true, shift);
-                            }, 5000);
+                                chart.series[0].addPoint([Date.now(), ss.routines.getPortfolioValue() + ss.data.player_data.cash], true, shift);
+                            }, 10000);
                         }
                     }
                 },
@@ -121,7 +150,10 @@ ss.pages = {
                 xAxis: {
                     type: 'datetime',
                     tickPixelInterval: 150,
-                    maxZoom: 20 * 1000
+                    maxZoom: 20 * 1000,
+                    labels: {
+                        enabled: false
+                    }
                 },
                 yAxis: {
                     minPadding: 0.2,
@@ -133,10 +165,190 @@ ss.pages = {
                 },
                 series: [{
                     name: 'Portfolio value',
-                    data: [[Date.now(), ss.routines.getPortfolioValue()]]
+                    data: [[Date.now(), ss.routines.getPortfolioValue() + ss.data.player_data.cash]]
                 }]
             });        
         });
+    },
+    detailed_overview: function() {
+
+        let stocks = ss.data.player_data.owned_stocks;
+
+        $(document).ready(function($) {
+            $(".clickable").click(function() {
+                window.document.location = "?r=stock_view&symbol=" + $(this).data("symbol");
+            });
+        });
+
+        for (var i = Object.keys(stocks).length - 1; i >= 0; i--) {
+
+            let code = Object.keys(stocks)[i],
+                row = '<tr style="cursor: pointer" class="clickable" data-symbol="' + code + '"><td>' + code + '</td>' + '<td>' + ss.data.company_data[code].name + '</td>' + '<td>' + ss.data.company_data[code].category + '</td><td>' + '$' + ss.data.company_data[code].last_price + '</td><td>' + stocks[code] + '</td><td>' + '$' + ss.data.company_data[code].last_price * stocks[code] + '</td></tr>';
+
+            if (stocks[code] == 0) {
+                continue;
+            };
+
+            $('#holdings > tbody:last-child').append(row);
+
+        }
+
+        $('#holdings > tbody:last-child').append('<tr><td></td><td>Cash</td><td>Liquid</td><td></td><td></td><td>' + '$' + ss.data.player_data.cash + '</td></tr>');
+
+        $('#holdings > tbody:last-child').append('<tr><td></td><td><strong>Total</strong></td><td></td><td></td><td></td><td>' + '$' + (ss.data.player_data.cash + ss.routines.getPortfolioValue()) + '</td></tr>');
+
+    },
+    stock_search: function() {
+
+        var stocks = [],
+            data = ss.data.company_data;
+
+        for (var i = Object.keys(data).length - 1; i >= 0; i--) {
+
+            let code = Object.keys(data)[i];
+
+            stocks.push(code + " – " + data[code].name);
+
+        }
+
+        var stocks = new Bloodhound({
+            datumTokenizer: Bloodhound.tokenizers.whitespace,
+            queryTokenizer: Bloodhound.tokenizers.whitespace,
+            // `stocks` is an array of state names defined in "The Basics"
+            local: stocks
+        });
+
+        $('#searcher .typeahead').typeahead({
+            hint: true,
+            highlight: true,
+            minLength: 1
+        },
+        {
+            name: 'stocks',
+            source: stocks
+        });
+
+        $("#searcher .typeahead").keyup(function(event){
+            if(event.keyCode == 13){
+                ss.routines.stock_search($('#search_field').val().substr(0,3))            
+            }
+        });
+
+    },
+    stock_view: function() {
+        var params={};window.location.search.replace(/[?&]+([^=&]+)=([^&]*)/gi,function(str,key,value){params[key] = value;});
+
+        if (typeof params.symbol === "undefined") {
+            alert("No symbol provided!");
+            window.location = '?r=stock_search';
+
+            throw new Error("No symbol provided in symbol arg.");
+        } else if (Object.keys(ss.data.company_data).indexOf(params.symbol) === -1) {
+            alert("Invalid symbol provided!");
+            window.location = '?r=stock_search';
+
+            throw new Error("Invalid symbol provided.");
+        }
+
+        $(document).ready(function() {
+            ss.data.internal.stock_chart = new Highcharts.Chart({
+                chart: {
+                    renderTo: 'symbol-graph',
+                    defaultSeriesType: 'spline',
+                    events: {
+                        load: function() { 
+                            setInterval(function() {
+                                let chart = ss.data.internal.stock_chart,
+                                    series = chart.series[0],
+                                    shift = series.data.length > 20;
+
+                                    $("#price").html(ss.data.company_data[params.symbol].last_price);
+                                    $("#cash").html(ss.data.player_data.cash);
+
+                                chart.series[0].addPoint([Date.now(), ss.data.company_data[params.symbol].last_price], true, shift);
+                            }, 10000);
+                        }
+                    }
+                },
+                title: {
+                    text: ' '
+                },
+                xAxis: {
+                    type: 'datetime',
+                    tickPixelInterval: 150,
+                    maxZoom: 20 * 1000,
+                    labels: {
+                        enabled: false
+                    }
+                },
+                yAxis: {
+                    minPadding: 0.2,
+                    maxPadding: 0.2,
+                    title: {
+                        text: ' ',
+                        margin: 80
+                    }
+                },
+                series: [{
+                    name: params.symbol + ' Value',
+                    data: [[Date.now(), ss.data.company_data[params.symbol].last_price]]
+                }]
+            });        
+        });
+
+        $("#symbol").html(params.symbol);
+        $("#name").html(ss.data.company_data[params.symbol].name);
+        $("#price").html(ss.data.company_data[params.symbol].last_price);
+        $("#cash").html(ss.data.player_data.cash);
+
+        if (typeof ss.data.player_data.owned_stocks[params.symbol] === "undefined") {
+            $("#owned_shares").html(0);
+            ss.data.player_data.owned_stocks[params.symbol] = 0;
+        } else {
+            $("#owned_shares").html(ss.data.player_data.owned_stocks[params.symbol]);
+        }
+
+        $("#purchase_amount").keyup(function(event){
+            var amount = $("#purchase_amount").val();
+
+            if (amount * ss.data.company_data[params.symbol].last_price > ss.data.player_data.cash || amount == 0) {
+                if (amount != 0) {
+                    $("#insufficent_purchase_amount").html(amount);
+                    $("#alert_insufficent_funds").show();
+                };
+
+                $("#submit_button").prop('disabled', true);
+            } else {
+                $("#alert_insufficent_funds").hide();
+                $("#submit_button").prop('disabled', false);
+
+                if(event.keyCode == 13){
+                    ss.routines.stock_purchase();          
+                }
+            }
+        });
+
+        $("#sell_amount").keyup(function(event){
+            var amount = $("#sell_amount").val();
+
+            if (amount > ss.data.player_data.owned_stocks[params.symbol] || amount == 0) {
+                if (amount != 0) {
+                    $("#insufficent_sell_amount").html(amount);
+                    $("#alert_insufficent_stocks").show();
+                };
+
+                $("#sell_button").prop('disabled', true);
+            } else {
+                $("#alert_insufficent_stocks").hide();
+                $("#sell_button").prop('disabled', false);
+
+                if(event.keyCode == 13){
+                    ss.routines.stock_sell();          
+                }
+            }
+        });
+
+
     }
 }
 
